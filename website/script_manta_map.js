014e1280f24f8e5aa31650fa4cb69b1d58e6eb9d
Yusuke Hatanaka <yusukemh@hawaii.edu>
2020-11-24 13:21:51 -1000

    temp

--- website/script_manta_map.js
+++ website/script_manta_map.js
@@ -6,13 +6,12 @@ function coordinate_parser(string) {
   }
   return ret
 }
-
-function createPopup(dataPoint){
+//INITIAL BULIT FOR THE MAP
+function createPopup(dataPoint) {
   //add image
   str = '<div><img src = '
   //str += 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Stack_Exchange_logo_and_wordmark.svg/375px-Stack_Exchange_logo_and_wordmark.svg.png'
   str += 'images/mantapics/' + dataPoint.ID.toString() + '.jpg';
-  console.log('images/mantapics/' + dataPoint.ID.toString() + '.jpg')
   str += ' height="200"><hr>';
   str += '<div>DATE: ' + dataPoint.Mo + '/' + dataPoint.Da + '/' + dataPoint.Yr + '<br>';
   str += 'Latitude: ' + dataPoint.Latitude + '<br>';
@@ -20,8 +19,8 @@ function createPopup(dataPoint){
   str += 'Time: '
   time = Number(dataPoint.Time)
   hour = Math.floor(time)
-  min = Math.round(6000 * (time - hour))/100
-  if(hour > 12){hour -= 12};
+  min = Math.round(6000 * (time - hour)) / 100
+  if (hour > 12) { hour -= 12 };
   str += hour.toString() + ':' + min.toString()
   str += time > 12 ? 'PM' : 'AM';
   str += '<br>Group Size: ' + dataPoint.Group_Size;
@@ -41,6 +40,7 @@ d3.csv('../data/clean_data.csv', function (data) {
     lon = coordinate_parser(data[i].Longitude);
     lat = coordinate_parser(data[i].Latitude);
     coordinates.push([lat, lon, num]);
+    var temp = L.divIcon({ popupAnchor: [100, 30] });
     circle = L.circle([lat, lon], {
       weight: 0.2,
       color: 'white',
@@ -49,27 +49,13 @@ d3.csv('../data/clean_data.csv', function (data) {
       radius: 16
     });
 
-
-    
-    // circle.on('mouseover', function (e) {
-    //   this.openPopup();
-    // });
-    // circle.on('mouseout', function (e) {
-    //   this.closePopup();
-    // });
-
-
     popupContent = document.createElement("img");
     popupContent = createPopup(data[i]);
-    //popupContent = "<div>HELLO WORLD<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Stack_Exchange_logo_and_wordmark.svg/375px-Stack_Exchange_logo_and_wordmark.svg.png'></div>"
-
-    
     circle.bindPopup(popupContent, {
       maxWidth: "auto"
     });
     circles.push(circle);
   }//iterate through each lat/long
-
 
   for (i = 0; i < circles.length; i++) {
     circles[i].addTo(mymap);
@@ -78,15 +64,31 @@ d3.csv('../data/clean_data.csv', function (data) {
 })
 
 var mymap = L.map('map_mantas').setView([21.48, -157.82], 14.);;
-// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-//   maxZoom: 20,
-//   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-//     '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-//     'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//   id: 'mapbox/WorldImagery',
-//   tileSize: 512,
-//   zoomOffset: -1
-// }).addTo(mymap);
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+  attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 }).addTo(mymap);
+
+$(function () {
+  $("#slider-range").slider({
+    range: true,
+    min: 0,
+    max: 28,
+    values: [0, 28],
+    slide: function (event, ui) {
+      $("#amount").val("Day " + ui.values[0] + " - Day " + ui.values[1]);
+      lower = ui.values[0];
+      upper = ui.values[1];
+    }
+  });
+  $("#amount").val("Day " + $("#slider-range").slider("values", 0) +
+    " - Day " + $("#slider-range").slider("values", 1));
+});
+
+
+var lower = 0;
+var upper = 28;
+function update(){
+  console.log('hello world')
+}
+
+
